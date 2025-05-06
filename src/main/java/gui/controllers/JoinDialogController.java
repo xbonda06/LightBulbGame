1b12958b5c8506e65efeaadfd958a37b2de55689
OlhaTmlk <tomilkoolha@gmail.com>
2025-05-06 20:51:10 +0200

    feat(gui): add multiplayer controllers and views

---
+++ src/main/java/gui/controllers/JoinDialogController.java
@@ -0,0 +1,15 @@
+package gui.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class JoinDialogController {
+    public Button joinButton;
+    public TextField textEnter;
+
+    @FXML
+    public void joinGame(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/gui/controllers/MultiplayerController.java
+++ src/main/java/gui/controllers/MultiplayerController.java
@@ -8,6 +8,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 
 public class MultiplayerController {
+
     @FXML public StackPane rootPane;
     @FXML public Label playerId;
     @FXML public GridPane gameGrid;
@@ -15,13 +16,46 @@ public class MultiplayerController {
     @FXML public Button redoButton;
     @FXML public Label stepsLabel;
     @FXML public Label timerLabel;
+    @FXML public Button createButton;
+    @FXML public Button joinGame;
+    @FXML public Button mainButton;
+    @FXML public Button startButton;
+    @FXML public Label ipAddress;
+    @FXML public Label playerCount;
 
-    @FXML public void toMainMenu(ActionEvent actionEvent) {
+    // From the game mode -> multiplayer_main.fxml
+
+    @FXML public void toMainMenu() {
     }
 
-    @FXML public void getRedo(ActionEvent actionEvent) {
+    @FXML public void getRedo() {
     }
 
-    @FXML public void getUndo(ActionEvent actionEvent) {
+    @FXML public void getUndo() {
+    }
+
+    // From the menu when choosing CREATE or JOIN -> multiplayer_menu.fxml
+
+    @FXML public void createGame() {
+    }
+
+    @FXML public void joinGame() {
+    }
+
+    @FXML public void toTheMain() {
+    }
+
+    // From waiting screen -> wait_for_connection.fxml
+
+    @FXML public void startGame() {
     }
 }
+
+//NEW GAME:
+//instance Server. Port random
+//instance client + connect
+
+//JOIN GAME:
+//IP and Port
+//instance client
+
