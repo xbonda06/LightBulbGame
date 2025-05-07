1b916220c44c4f141b41d034ec01c435460ef283
OlhaTmlk <tomilkoolha@gmail.com>
2025-05-07 09:36:42 +0200

    feat(gui): сleaned up the code by removing unnecessary parts.

--- src/main/java/gui/controllers/JoinDialogController.java
+++ src/main/java/gui/controllers/JoinDialogController.java
@@ -1,6 +1,5 @@
 package gui.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
--- src/main/java/gui/controllers/MultiplayerController.java
+++ src/main/java/gui/controllers/MultiplayerController.java
@@ -1,6 +1,5 @@
 package gui.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,11 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
