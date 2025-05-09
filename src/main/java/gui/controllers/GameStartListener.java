30df34f608b921818299f6b79cffede75fc1259a
xbonda06 <mediabondar@gmail.com>
2025-05-09 17:25:28 +0200

    docs: add javadoc comments to CallBacks

--- src/main/java/gui/controllers/GameStartListener.java
+++ src/main/java/gui/controllers/GameStartListener.java
@@ -1,5 +1,17 @@
+/**
+ * Listener interface for receiving a callback when the game starts.
+ *
+ * <p>Use this interface to perform actions (e.g., switching views, enabling controls)
+ * when a multiplayer game session begins.</p>
+ *
+ * @author Andrii Bondarenko (xbonda06)
+ */
 package gui.controllers;
 
 public interface GameStartListener {
+
+    /**
+     * Called when the game has officially started.
+     */
     void onGameStarted();
 }
--- src/main/java/gui/controllers/GameWinListener.java
+++ src/main/java/gui/controllers/GameWinListener.java
@@ -1,5 +1,19 @@
+/**
+ * Listener interface for receiving a callback when a player wins the game.
+ *
+ * <p>Useful in multiplayer scenarios to display victory messages or trigger
+ * post-game logic such as disabling the board or showing a summary screen.</p>
+ *
+ * @author Andrii Bondarenko (xbonda06)
+ */
 package gui.controllers;
 
 public interface GameWinListener {
+
+    /**
+     * Called when a player wins the game.
+     *
+     * @param winnerId the ID of the player who won
+     */
     void onGameWin(int winnerId);
 }
